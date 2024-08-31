ebf685ed417d1f32d5c0f86131932f67cc870f90
Mehul Agarwal <mehu2003@gmail.com>
2024-08-31 20:18:22 +0530

    change frontend for deploy

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
-const backendUrl = "http://localhost:5000/api/restaurants";
+const backendUrl = window.location.hostname === "localhost" ? "http://localhost:5000/api/restaurants" : "https://restaurant-search-qj4m.onrender.com/api/restaurants";
 
 export {backendUrl};
